58c16e3d5b3e94c565c92aba9f4eafa5e9ad3f94
Davide Perozzi <triplexp@live.de>
2015-12-23 15:45:14 +0100

    UPDATE default screen stylings and texts FIX some translations

--- translations/en/layout/default/meta.php
+++ translations/en/layout/default/meta.php
@@ -1,5 +1,5 @@
 <?php
 
 return array(
-	'title' => 'Your first application'
+	'title' => 'YEAH! - It\'s working'
 );
--- translations/en/view/index/index.php
+++ translations/en/view/index/index.php
@@ -1,6 +1,21 @@
 <?php
 
 return array(
-	'h1' => 'fewlines framework',
-	'text' => 'YEAH - It\'s working :) <br /> Now you can create your project.'
+	'h1' => 'fewlines',
+	'text' => 'YEAH! - It\'s working...<br /><br />
+			   Now you are ready too create your own project by using the following command (from the root directory): <br /><br />',
+	'command' => './fewlines project:create [--activate] <id> <namespace> [<name>] [<description>]',
+	'example' => array(
+		'text' => 'Example:<br />',
+		'command' => './fewlines project:create --activate yourproject "YourProject"'
+	),
+	'project' => array(
+		'registered' => 'You have already registered at least 1 project. <br />
+					     To activate a project use the following command:<br /><br />',
+		'command' => './fewlines project:activate <id>',
+		'example' => array(
+			'text' => 'Example:<br />',
+			'command' => './fewlines project:activate %s'
+		)
+	)
 );
